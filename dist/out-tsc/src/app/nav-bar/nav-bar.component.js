e937c004674f7a209de7cd8e60d44afe7d0738b4
udaykumar572 <60086619+udaykumar572@users.noreply.github.com>
2020-02-20 18:17:52 +0530

    Add files via upload

---
+++ dist/out-tsc/src/app/nav-bar/nav-bar.component.js
@@ -0,0 +1,16 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+let NavBarComponent = class NavBarComponent {
+    constructor() { }
+    ngOnInit() {
+    }
+};
+NavBarComponent = tslib_1.__decorate([
+    Component({
+        selector: 'app-nav-bar',
+        templateUrl: './nav-bar.component.html',
+        styleUrls: ['./nav-bar.component.css']
+    })
+], NavBarComponent);
+export { NavBarComponent };
+//# sourceMappingURL=nav-bar.component.js.map
